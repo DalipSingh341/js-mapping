a9e218af5ca103f5c13af4355524322df86000d5
Dipakkkk <dr916139@gmail.com>
2023-09-05 13:56:22 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //  function Deep(param1, param2) {
+  //    var a = param1 + "love" + param2;
+  //    return a;
+  //  }
+  //  console.log(Deep("Me", "You"));;
+
+  //   var a = "global";
+  //   function first() {
+  //     var a = "fresh";
+  //     return a;
+  //     function second() {
+  //       console.log(a);
+  //     }
+  //   }
+  //   console.log(first(a));
+  //   console.log(a);
+  //
+
+  // var a;
+  // a = "me";
+  // a = 12;
+  // console.log(a);
+  // var a = "me";
+
+  // var a;
+  // a = 2 + "7";
+  // a = 2 + +"7";
+  // console.log(a);
+
+  // var a = 12;
+  // var b ="12"
+  // console.log(a = b);
+  // 12
+  // console.log(a == b);
+  // true
+  // console.log(a === b);
+  // false
+
+  // var a;
+  // a = true - 100;
+  // console.log(a);
+
+  //   const time = data.Hourly
+  //   var a = 12;
+
+  //   if (a > 10) {
+  //     time = "good morning";
+  //   }
+  //   // else if (a > 20) {
+  //   //   time = "good afternoon";
+  //   // } else {
+  //   //   time = "good night";
+  //   // }
+  //   console.log(data.Hourly);
+  //
+
+  // function Deep(a) {
+  //   var result;
+  //   if (a > 0) {
+  //     result = "positive";
+  //   }
+  //   else {
+  //     result="negative"
+  //   }
+  //   return result;
+  // }
+  // console.log(Deep(-2));
+  // console.log(Deep(2));
+
+  //1st- negative
+  //2nd-positive
+
+  const Sdata = [
+    {
+      id: 1,
+      name: "Deepak",
+      degree: "bCom",
+    },
+    {
+      id: 2,
+      name: "Pardeep",
+      degree: "Bsc",
+    },
+    {
+      id: 3,
+      name: "Robin",
+      degree: "BA",
+    },
+  ];
+  // console.log(Sdata[2]);
+  // console.log(Sdata[0].name);
+
+  const NewData = Sdata.map((cvalue) => {
+    return `My name is ${cvalue.name}. My higher Education is ${cvalue.degree}` ;
+  });
+  document.getElementById("ShowData").innerHTML=NewData;
+  console.log(NewData); 
+  
+  
 }
 
 export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const map = () => {
+    function Deep(param1, param2) {
+      var a = param1 + "love" + param2;
+      return a;
+    }
+  Deep("Me", "You");
+  console.log(Deep(a))
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default map
